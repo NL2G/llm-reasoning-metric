067c25572198dc5c22157c65b360de05cc9c113b
Daniil Larionov <rexhaif.io@gmail.com>
2025-04-18 15:41:05 +0200

    in-progress

--- training/metric_utils.py
+++ training/metric_utils.py
@@ -1,5 +1,9 @@
 import re
-import sys
+from sympy.parsing.latex import parse_latex
+import json
+from ifeval_functions import IF_FUNCTIONS_MAP
+
+
 DEFAULT_INSTRUCTION = "Translate this text from {source_language} to {target_language}, maintaining the tone, accuracy and ensuring fluency: {source_text}"
 
 JUDGE_PROMPT = """Please act as an impartial judge and evaluate the quality of the translations provided by two AI assistants in response to the user's request below.
@@ -64,6 +68,7 @@ def pairwise_ranking_grpo_transform(cfg, *args, **kwargs):
                 {"role": "user", "content": input_message},
             ],
             "answer": answer,
+            "dataset": "mt-ranking"
         }
 
     return transform_fn, {
@@ -72,6 +77,18 @@ def pairwise_ranking_grpo_transform(cfg, *args, **kwargs):
             "ref", 'score0', 'score1', 'score_diff',
             "score_name", "system0", "system1"
         ],
+    }
+
+def tulu_transform(cfg, *args, **kwargs):
+    def transform_fn(example, tokenizer=None):
+        return {
+            "prompt": example["messages"],
+            "answer": example["ground_truth"],
+            "dataset": example["dataset"]
+        }
+
+    return transform_fn, {
+        'remove_columns': ['messages', 'ground_truth', 'constraint_type', 'constraint']
     }
 
 
@@ -83,6 +100,15 @@ def extract_answer(response):
         answer = "NOTHING"
     # remove [] if present
     return answer.replace('[', '').replace(']', '')
+
+
+def extract_thinking(response):
+    thinking = re.search(r'<think>(.*?)</think>', response, re.DOTALL)
+    if thinking:
+        thinking = thinking.group(1).strip()
+    else:
+        thinking = "NOTHING"
+    return thinking
 
 
 def count_xml(text) -> float:
@@ -100,11 +126,54 @@ def count_xml(text) -> float:
     return count
 
 
-def reward_answer_correctness(completions, answer, **kwargs):
-    answers = [extract_answer(completion[0]['content']) for completion in completions]
-    return [
-        2.0 if answers[i] == answer[i] else 0.0 for i in range(len(answers))
-    ]
+def string_exact_match(extracted_answer, ground_truth):
+    return extracted_answer == ground_truth
+
+def number_exact_match(extracted_answer, ground_truth):
+    try:
+        extracted_answer = float(extracted_answer.replace(",", "."))
+        ground_truth = float(ground_truth.replace(",", "."))
+        return extracted_answer == ground_truth
+    except ValueError:
+        return False
+    
+def latex_exact_match(extracted_answer, ground_truth):
+    try:
+        extracted_answer = parse_latex(extracted_answer).simplify()
+        ground_truth = parse_latex(ground_truth).simplify()
+        return extracted_answer.equals(ground_truth)
+    except Exception:
+        return False
+    
+
+def ifeval_validator(extracted_answer, ground_truth):
+    if isinstance(ground_truth, str):
+        constraint = json.loads(ground_truth)
+    else:
+        constraint = ground_truth
+
+    func_name = constraint.pop("func_name")
+    func = IF_FUNCTIONS_MAP[func_name]
+    non_none_args = {k: v for k, v in constraint.items() if v is not None}
+    func_result = func(extracted_answer, **non_none_args)
+    return func_result
+
+REWARD_FUNCTIONS = {
+    "mt-ranking": string_exact_match,
+    "gsm8k": number_exact_match,
+    "MATH": latex_exact_match,
+    "ifeval": ifeval_validator
+}
+
+def reward_answer_correctness(completions, answer, dataset, **kwargs):
+    pred_answers = [extract_answer(completion[0]['content']) for completion in completions]
+    rewards = []
+    for pred_answer, true_answer, item_dataset in zip(pred_answers, answer, dataset):
+        if item_dataset not in REWARD_FUNCTIONS:
+            raise ValueError(f"Unknown dataset: {item_dataset}")
+        reward = 2.0 if REWARD_FUNCTIONS[item_dataset](pred_answer, true_answer) else 0.0
+        rewards.append(reward)
+    return rewards
 
 
 def reward_format_correctness(completions, **kwargs):
@@ -112,8 +181,3 @@ def reward_format_correctness(completions, **kwargs):
         count_xml(completion[0]['content']) for completion in completions
     ]
 
-def choice_reward(completions, **kwargs):
-    return [
-        0.5 if extract_answer(completion[0]['content']) in {'A', 'B'} else 0.0
-        for completion in completions
-    ]
